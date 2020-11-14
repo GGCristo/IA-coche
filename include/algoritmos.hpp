56b8a1ad5d57aa6e5d8d5bedd1c928375a1be55a
Cristo García <alu0101204512@ull.edu.es>
2020-11-14 20:22:00 +0000

    Cambios Edu heurística, sin comprobar

--- include/algoritmos.hpp
+++ include/algoritmos.hpp
@@ -6,7 +6,7 @@
 #include "Celda.hpp"
 #include "Malla.hpp"
 extern int opcion_heuristico;
-std::vector<Celda*> Heuristica(std::vector<Celda*>& vector_desordenado);
+void Heuristica(std::vector<Celda*>& vector_desordenado);
 bool mysort(const std::pair<Celda*, float> &a, const std::pair<Celda*, float> &b);
 void Elprimeromejor();
 void ElMejor(std::vector<Celda*>& cola, std::vector<Celda*>::iterator& it);
